a906666ee511650d389d1fa566e1e14f0008e4a3
Michal Srb <srb@convex.dev>
2023-09-19 17:27:38 -0700

    Fix UI

--- app/(fullstack)/page.tsx
+++ app/(fullstack)/page.tsx
@@ -86,12 +86,12 @@ function AuthForm() {
       signUp={signUp}
       labelClassName="mb-4"
       inputClassName="
-          flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1
+          flex h-9 rounded-md border border-input bg-transparent px-3 py-1
           text-sm shadow-sm transition-colors file:border-0 file:bg-transparent
           file:text-sm file:font-medium placeholder:text-muted-foreground
           focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring
           disabled:cursor-not-allowed disabled:opacity-50
-          mb-4 w-80"
+          mb-4 w-[18rem]"
       buttonClassName="
           inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors
           focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring
@@ -99,7 +99,7 @@ function AuthForm() {
           bg-primary text-primary-foreground shadow hover:bg-primary/90
           h-9 px-4 py-2
           cursor-pointer w-full mb-4"
-      flowToggleClassName="block font-medium text-primary cursor-pointer text-center"
+      flowToggleClassName="block font-medium text-primary cursor-pointer text-center hover:underline underline-offset-4"
     />
   );
 }
